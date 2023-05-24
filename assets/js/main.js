1824f2c25fd5919aec0d522bdac495a509f66751
FaliPixelatto <fali@pixelatto.com>
2023-05-24 14:42:59 +0200

    Web ready

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+﻿function CreateGameiFrame(gameId, gameUrl) {
+    var f = document.createElement('iframe');
+    f.src = "assets/games/" + gameUrl;
+    f.width = 1000;
+    f.height = 500;
+    f.id = "iframe-" + gameId;
+    $("#game-" + gameId).append(f);
+}
+
+function RemoveGameiFrame(gameId) {
+    $("#iframe-" + gameId).remove();
+}
+
+function EscapePressed(e, gameId, closeButton) {
+    if(e.key === "Escape") {
+        RemoveGameiFrame(gameId);
+        closeButton.click();
+    }
+}
